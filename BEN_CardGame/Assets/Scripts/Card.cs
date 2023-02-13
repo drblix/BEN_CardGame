5abac15aaf24a2aefc635c61db951d732a2f93e8
Ben <91279378+drblix@users.noreply.github.com>
2023-02-13 11:22:23 -0500

    Update Card.cs

--- BEN_CardGame/Assets/Scripts/Card.cs
+++ BEN_CardGame/Assets/Scripts/Card.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    // Declarations
     public CardSlot filledSpot;
     private AudioSource source;
 
@@ -11,15 +12,14 @@ public class Card : MonoBehaviour
 
     private const float MOVE_SPEED = 24f;
     private const float FLIP_SPEED = 3f;
-
-
+    
     private DrawingDeck.CardNumber number = DrawingDeck.CardNumber.One;
     private DrawingDeck.CardColor color = DrawingDeck.CardColor.Red;
     
     public DrawingDeck.CardNumber Number { get { return number; } }
     public DrawingDeck.CardColor Color { get { return color; } }
 
-    // false means logo is showing, true means number and color is showing
+    // False means logo is showing, true means number and color is showing
     private bool flipped = false;
     public bool Flipped { get { return flipped; } }
     private bool isFlipping = false;
@@ -47,15 +47,19 @@ public class Card : MonoBehaviour
     /// </summary>
     public IEnumerator FlipCard() 
     {
+        // Returns early if already flipping
         if (isFlipping) { yield break; }
-
+        
         isFlipping = true;
+        // declarations
         float timer = 0f;
         float rotAmnt = transform.eulerAngles.y;
 
         source.Play();
-
+        
+        // Loop that continues until 1 second has passed
         while (timer < 1f) {
+            // Linearally interpolates the rotational value and applies it as a quaternion rotation to the card object
             if (!flipped) {
                 rotAmnt = Mathf.Lerp(FLIPPED_VALUE, UNFLIPPED_VALUE, timer * FLIP_SPEED);
             }
@@ -64,11 +68,14 @@ public class Card : MonoBehaviour
             }
 
             transform.rotation = Quaternion.Euler(0f, rotAmnt, 0f);
+            
             timer += Time.deltaTime;
+            // Waits for frame to pass; stops animation from being instant
             yield return new WaitForEndOfFrame();
         }
 
         flipped = !flipped;
+        // Sets rotation to goal value
         transform.rotation = flipped ? Quaternion.Euler(0f, UNFLIPPED_VALUE, 0f) : Quaternion.Euler(0f, FLIPPED_VALUE, 0f);
         isFlipping = false;
     }
@@ -76,10 +83,11 @@ public class Card : MonoBehaviour
     // Moves the card to a position overtime
     public IEnumerator MoveCard(Vector3 to, bool leaveSpot) 
     {
-        // Returns if already moving
+        // Returns early if already moving
         if (isMoving) { yield break; }
         isMoving = true;
-
+        
+        // Clears filledSpot variable if 'leaveSpot' is true
         if (leaveSpot && filledSpot) { 
             filledSpot.Filled = false;
             filledSpot = null;
@@ -95,14 +103,17 @@ public class Card : MonoBehaviour
         transform.position = to;
         isMoving = false;
     }
-
+    
+    // Debugging
     public override string ToString()
     {
         return "Color: " + color + " || Number: " + number;
     }
-
+    
+    // Used for wild card instances
     public void SetColor(DrawingDeck.CardColor c) => color = c;
-
+    
+    // Overriden equals function to simplify comparisons
     public bool Equals (Card card) 
     {
         if (!card || this.Number == DrawingDeck.CardNumber.Wild)
